d50538a87a91e41937296aa987a891470c410657
fabcodingzest <fabrizvi786@gmail.com>
2019-11-19 18:20:15 +0530

    Custom Hook: Reducer + LocalStorage

--- src/Context/todos.context.js
+++ src/Context/todos.context.js
@@ -1,8 +1,9 @@
 //todos
 //all methods to interact w/ todos
 
-import React, { createContext, useReducer } from "react";
+import React, { createContext } from "react";
 import { todoReducer } from "../reducers/todo.reducer";
+import { useLocalStorageReducer } from "../Hooks/useLocalStorageReducer";
 
 const defaultTodos = [
   { id: 1, task: "Avera ke davera", completed: false },
@@ -12,7 +13,11 @@ export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
-  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
+  const [todos, dispatch] = useLocalStorageReducer(
+    "todos",
+    defaultTodos,
+    todoReducer
+  );
   return (
     <TodosContext.Provider value={todos}>
       <DispatchContext.Provider value={dispatch}>
---
+++ src/Hooks/useLocalStorageReducer.js
@@ -0,0 +1,21 @@
+import { useReducer, useEffect } from "react";
+
+function useLocalStorageReducer(key, defaultVal, reducer) {
+  //make piece of state, based off of value in localStorage or default
+  const [state, dispatch] = useReducer(reducer, defaultVal, () => {
+    let val;
+    try {
+      val = JSON.parse(window.localStorage.getItem(key) || String(defaultVal));
+    } catch (e) {
+      val = defaultVal;
+    }
+    return val;
+  });
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [state]);
+
+  return [state, dispatch];
+}
+
+export { useLocalStorageReducer };
